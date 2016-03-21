24963852c61511794aed0a9ad087940d7e297171
Sergio Garcia <srgarcia91@gmail.com>
2016-03-21 20:41:15 +0100

    Created Tile class

---
+++ Point Mazes/Assets/Scripts/MyGameScripts/Board/Tile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction {
+	Top = 0,
+	Left = 1,
+	Bottom = 2,
+	Right = 3
+};
+
+public class Tile : MonoBehaviour {
+
+	private GameObject reference;
+
+	private Tile[] neighbours;
+	private bool[] walls;
+
+	public Tile(GameObject reference) {
+		this.reference = reference;
+		neighbours = new Tile[4];
+		walls = new bool[4];
+	}
+
+	/* Set the neighbours of the Tile */
+	public void SetNeighbours(Tile top, Tile left, Tile bottom, Tile right) {
+		neighbours[(int) Direction.Top] = top;
+		neighbours[(int) Direction.Left] = left;
+		neighbours[(int) Direction.Bottom] = bottom;
+		neighbours[(int) Direction.Right] = right;
+	}
+
+	/* Set the value of the wall in the Tile */
+	public void SetWall(Direction dir, bool state) {
+		walls[(int) dir] = state;
+	}
+}
